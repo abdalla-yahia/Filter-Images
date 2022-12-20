f123f03706d9e0e8f69bd297ce29f1ea3142c79a
Abdalla Yahia Kamel <94574716+abdalla-yahia@users.noreply.github.com>
2022-12-20 19:35:46 +0200

    add invert filter

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
-const img = document.getElementById("img-upload");
-const canvas = document.getElementById("canvas");
-const upload = document.getElementById("upload");
-
-const Saturate = document.getElementById("Saturate");
-const Contrast = document.getElementById("Contrast");
-const Brightness = document.getElementById("Brightness");
-const Sepia = document.getElementById("Sepia");
-const GrayScale = document.getElementById("GrayScale");
-const Blur = document.getElementById("Blur");
+const upload = document.getElementById("upload")
+const img = document.images[0]
+const container =document.querySelector(".container")
+const blur = document.getElementById("blur");
+const saturate = document.getElementById("saturate");
+const brightness = document.getElementById("brightness");
+const contrast = document.getElementById("contrast");
+const sepia = document.getElementById("sepia");
+const grayscale = document.getElementById("grayscale");
 const hue_rotate = document.getElementById("hue-rotate");
-const range = document.querySelectorAll("input[type='range']")
-const download = document.getElementById("download");
+const invert = document.getElementById("invert");
+
 const reset = document.getElementById("reset");
+const download = document.getElementById("download");
+download.style.display = "none"
+reset.style.display = "none"
+const ranges = document.querySelectorAll('input[type=range')
+let canvas = document.getElementById("canvas")
 
-const ctx = canvas.getContext('2d');
+let ctx = canvas.getContext('2d')
 
-
-window.onload = () => {
-    download.style.display ="none"
-    reset.style.display ="none"
+function resetDefault() {
+    ctx.filter="none"
+    blur.value = 0;
+    saturate.value = 100;
+    brightness.value =1
+    contrast.value = 100
+    sepia.value =0
+    grayscale.value =0
+    hue_rotate.value =0
+    invert.value = 0;
+    ctx.drawImage(img,0,0,canvas.width,canvas.height)
 }
-function resetValue() {
-        ctx.filter = "none"
-        Saturate.value = 100
-        Contrast.value =100
-        Brightness.value =100
-        Sepia.value =0
-        GrayScale.value=0
-        Blur.value =0
-        hue_rotate.value=0
-        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-}
+
 upload.onchange = () => {
-    resetValue()
-
-    download.style.display = "block";
-    reset.style.display = "block";
-    let file = new FileReader()
+    download.style.display = "block"
+reset.style.display = "block"
+    resetDefault()
+    let file =new FileReader()
     file.readAsDataURL(upload.files[0])
     file.onload = () => {
         img.src = file.result;
@@ -44,27 +44,31 @@ upload.onchange = () => {
     img.onload = () => {
         canvas.width = img.width;
         canvas.height = img.height
-        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-        img.style.display='none'
+        ctx.drawImage(img,0,0,canvas.width,canvas.height)
     }
+    img.style.display = 'none'
 }
-range.forEach(e => {
-    e.addEventListener('input',function(){
-        ctx.filter = 
-        `saturate(${Saturate.value}%)
-        contrast(${Contrast.value}%)
-        brightness(${Brightness.value}%)
-        sepia(${Sepia.value}%)
-        grayscale(${GrayScale.value})
-        blur(${Blur.value}px)
+
+ranges.forEach(e => {
+    e.addEventListener('input', () => {
+        ctx.filter = `
+        blur(${blur.value}px)
+        saturate(${saturate.value}%)
+        brightness(${brightness.value})
+        contrast(${contrast.value}%)
+        sepia(${sepia.value})
+        grayscale(${grayscale.value})
         hue-rotate(${hue_rotate.value}deg)
+        invert(${invert.value})
         `
-        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+        ctx.drawImage(img,0,0,canvas.width,canvas.height)
     })
 })
 
-reset.onclick = resetValue;
-
-download.addEventListener('click', () => {
+download.onclick = () => {
     download.href = canvas.toDataURL('image/jpeg')
-})
+}
+
+reset.onclick = () => {
+    resetDefault()
+}
